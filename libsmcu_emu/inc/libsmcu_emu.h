5a7bc7eafd1146fa548a961d75f9b78b7ebc3e7b
Paul Soucy <paul@dev-smart.com>
2021-11-01 23:00:36 -0400

    wip

--- libsmcu_emu/inc/libsmcu_emu.h
+++ libsmcu_emu/inc/libsmcu_emu.h
@@ -4,13 +4,15 @@
 #include <stdint.h>     /* C99 types*/
 #include <stddef.h>
 
+#include <loragw_hal.h>
+
 struct smcu_emu {
   uint8_t key_pub[32];
-  uint8_t key_priv[32];
+  uint8_t key_priv[64];
 };
 
 int smcu_emu_init(struct smcu_emu* p, uint8_t* key_pub, uint8_t* key_priv);
 
-int smcu_emu_sign(struct smcu_emu* p, uint8_t* sig, uint8_t* data, size_t data_len);
+int smcu_emu_sign(struct smcu_emu* p, uint8_t* sig, struct lgw_pkt_rx_s* pkt);
 
 #endif // _SMCU_EMU_H
