c2bec53e3c19bee06475a3f88099385456fd5860
Paul Soucy <paul@dev-smart.com>
2021-11-08 19:27:17 -0500

    wip

--- smcu/src/lib.rs
+++ smcu/src/lib.rs
@@ -14,7 +14,7 @@ use ed25519_dalek::{Keypair, PublicKey, SecretKey, Signer};
 use rand::rngs::OsRng;
 use toml::{self};
 
-use serde::{de, Serialize, Deserialize};
+use serde::{de, Serialize, Deserialize, ser::Serializer};
 
 use log::{info, warn, error};
 
@@ -24,11 +24,15 @@ pub const SMCU_OK: i32 = 0;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
-    #[serde(deserialize_with = "deserialize_b58_pub")]
-    pub_key: PublicKey,
-
+    #[serde(serialize_with = "serialize_b58")]
     #[serde(deserialize_with = "deserialize_b58_priv")]
-    priv_key: SecretKey,
+    secret_key: SecretKey,
+}
+
+fn serialize_b58<S, T>(input: &T, serializer: S) -> Result<S::Ok, S::Error>
+where S: Serializer,
+T: AsRef<[u8]> {
+    serializer.serialize_str(&bs58::encode(input).into_string())
 }
 
 fn deserialize_b58_pub<'de, D>(deserializer: D) -> Result<PublicKey, D::Error>
@@ -78,7 +82,7 @@ fn read_config_from_file<P: AsRef<Path>>(path: P) -> std::io::Result<Config> {
 fn write_config_file<P: AsRef<Path>>(path: P, config: &Config) -> std::io::Result<()> {
     let config_str = toml::to_string_pretty(config)
                                 .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    let file = File::open(path)?;
+    let file = File::create(path)?;
     let mut writer = BufWriter::new(file);
     writer.write_all(config_str.as_bytes())
 }
@@ -92,8 +96,8 @@ fn smcu_init(smcu_ptr: &mut *mut SMCU) -> i32 {
         Ok(config) => {
             Box::new(SMCU {
                 keypair: Keypair {
-                    public: config.pub_key,
-                    secret: config.priv_key,
+                    public: PublicKey::from(&config.secret_key),
+                    secret: config.secret_key,
                 }
             })
         },
@@ -102,15 +106,21 @@ fn smcu_init(smcu_ptr: &mut *mut SMCU) -> i32 {
             error!("error reading config file: {}", e);
             let mut csprng = OsRng{};
             let keypair = Keypair::generate(&mut csprng);
+
+            let config = Config {
+                secret_key: keypair.secret,
+            };
             
 
-            write_config_file(CONFIG_FILE_PATH, &Config {
-                pub_key: keypair.public,
-                priv_key: keypair.secret,
-            });
+            if let Err(e) = write_config_file(CONFIG_FILE_PATH, &config) {
+                error!("error writing config file: {}", e);
+            }
 
             Box::new(SMCU {
-                keypair: keypair,
+                keypair: Keypair {
+                    public: keypair.public,
+                    secret: config.secret_key,
+                },
             })
 
         }
@@ -144,9 +154,26 @@ fn smcu_sign(smcu_ptr: *mut SMCU, message_str: *const c_char, pkt_ptr: *const Lo
 #[cfg(test)]
 mod tests {
     use super::*;
+    use simple_logger::SimpleLogger;
 
     #[test]
     fn it_works() {
+        SimpleLogger::new().init().unwrap();
+        let mut smcu = 0 as *mut SMCU;
+        smcu_init(&mut smcu);
+
+        smcu_free(smcu);
         
+    }
+
+    #[test]
+    fn derived_is_same() {
+        let mut csprng = OsRng{};
+        let keypair = Keypair::generate(&mut csprng);
+
+        let derive_pub = PublicKey::from(&keypair.secret);
+
+        assert_eq!(derive_pub, keypair.public);
+
     }
 }
